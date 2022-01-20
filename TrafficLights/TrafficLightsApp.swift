4d0e9e842b15a74b9cc88e8753fa5feb325d187c
Mikhail.Zinovev <mikhail.zinovev@russianpost.ru>
2022-01-20 09:40:16 +0300

    add empty app

---
+++ TrafficLights/TrafficLightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsApp.swift
+//  TrafficLights
+//
+//  Created by Михаил Зиновьев on 20.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
